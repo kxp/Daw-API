65e79531c0886992e22e6d68e5eb86f2e73bba49
RCardoso145 <ricardo.j.g.cardoso@gmail.com>
2019-04-06 13:44:05 +0100

    added blank project

---
+++ dawproject/src/main/kotlin/pt/isel/daw/LI61N/g10/dawproject/DawprojectApplication.kt
@@ -0,0 +1,11 @@
+package pt.isel.daw.LI61N.g10.dawproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DawprojectApplication
+
+fun main(args: Array<String>) {
+	runApplication<DawprojectApplication>(*args)
+}
---
+++ dawproject/src/test/kotlin/pt/isel/daw/LI61N/g10/dawproject/DawprojectApplicationTests.kt
@@ -0,0 +1,16 @@
+package pt.isel.daw.LI61N.g10.dawproject
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class DawprojectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
