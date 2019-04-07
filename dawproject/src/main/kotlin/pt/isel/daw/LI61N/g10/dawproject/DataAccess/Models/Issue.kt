2033512d64192e1777ad540715f9226503efb6e9
RCardoso145 <ricardo.j.g.cardoso@gmail.com>
2019-04-07 17:27:43 +0100

    added state interface and repository

---
+++ dawproject/src/main/kotlin/pt/isel/daw/LI61N/g10/dawproject/DataAccess/Models/Issue.kt
@@ -0,0 +1,12 @@
+package pt.isel.daw.LI61N.g10.dawproject.DataAccess.Models
+
+import java.util.*
+
+data class Issue (
+        val number : Int,
+        var name :String,
+        var short_desc : String,
+        var creation_date : Date,
+        var close_date : Date,
+        val state_id : String,
+        val project_id: Int)
