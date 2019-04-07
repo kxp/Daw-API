b55f22c222567ab3282a2bdc37f78135bf91b066
RCardoso145 <ricardo.j.g.cardoso@gmail.com>
2019-04-07 18:06:17 +0100

    added issues repository

---
+++ dawproject/src/main/kotlin/pt/isel/daw/LI61N/g10/dawproject/DataAccess/Repositories/IssueRepository.kt
@@ -0,0 +1,62 @@
+package pt.isel.daw.LI61N.g10.dawproject.DataAccess.Repositories
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.dao.EmptyResultDataAccessException
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import org.springframework.stereotype.Repository
+import pt.isel.daw.LI61N.g10.dawproject.DataAccess.Contracts.IIssueDataAccess
+import pt.isel.daw.LI61N.g10.dawproject.DataAccess.Contracts.IProjectDataAccess
+import pt.isel.daw.LI61N.g10.dawproject.DataAccess.Models.Issue
+import pt.isel.daw.LI61N.g10.dawproject.DataAccess.RowMappers.IssueRM
+import pt.isel.daw.LI61N.g10.dawproject.DataAccess.RowMappers.ProjectRM
+
+@Repository
+class IssueRepository: IIssueDataAccess {
+
+    private val SQL_FIND_BY_PROJECT_ID = "SELECT * FROM [dbo].[Issues] WHERE project_id = :project_id"
+    private val SQL_FIND_BY_ID = "SELECT * FROM [dbo].[Issues] WHERE number = :number"
+    private val SQL_INSERT = "INSERT INTO [dbo].[Issues] ([name], [short_desc], [creation_date], [close_date], [state_id], [project_id]) values(:name, :short_desc, :creation_date, :close_date, :state_id, :project_id)"
+    private val SQL_DELETE_BY_ID = "DELETE FROM [dbo].[Issues] WHERE [number] = :number"
+
+    private val ROW_MAPPER = IssueRM()
+
+    @Autowired
+    var jdbcTemplate: NamedParameterJdbcTemplate? = null
+
+    override fun getProjectIssue(issue_number: Int?): Issue? {
+        try {
+            val paramSource = MapSqlParameterSource("number", issue_number)
+            return jdbcTemplate!!.queryForObject(SQL_FIND_BY_ID, paramSource, ROW_MAPPER)
+        } catch (ex: EmptyResultDataAccessException) {
+            return null
+        }
+    }
+
+    override fun createProjectIssue(issue: Issue): Int {
+        val paramSource = MapSqlParameterSource()
+                .addValue("number", issue.number)
+                .addValue("name", issue.name)
+                .addValue("short_desc", issue.short_desc)
+                .addValue("creation_date", issue.creation_date)
+                .addValue("close_date", issue.close_date)
+                .addValue("state_id", issue.state_id)
+                .addValue("project_id", issue.project_id)
+
+        return jdbcTemplate!!.update(SQL_INSERT, paramSource)
+    }
+
+    override fun getProjectIssues(project_id: Int?): Iterable<Issue>? {
+        try {
+            val paramSource = MapSqlParameterSource("project_id", project_id)
+            return jdbcTemplate!!.query(SQL_FIND_BY_PROJECT_ID, paramSource, ROW_MAPPER)
+        } catch (ex: EmptyResultDataAccessException) {
+            return null
+        }
+    }
+
+    override fun deleteIssue(number: Int?) {
+        val paramSource = MapSqlParameterSource("number", number)
+        jdbcTemplate!!.update(SQL_DELETE_BY_ID, paramSource)
+    }
+}
