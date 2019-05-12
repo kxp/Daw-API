ba8f39c1f8c73f3c2f13e214193f59d2860cd8f3
Florin <sofinetflorin@gmail.com>
2019-05-13 00:42:00 +0100

    Played a bit with a login interface and routes.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Link, Switch, Route} from 'react-router-dom'
+import logo from './logo.svg'
+import Login from './components/Login'
+import Admin from './components/Admin'
+import Logout from './components/Logout'
 
+
+//if you remove exact in the route you can acces a path like this http://localhost:3000/b/asda
 function App() {
+  return (
+    <Switch>
+      <Route exact path="/" component={Login} /> 
+      <Route exact path="/admin" component={Admin} />
+      <Route exact path="/logout" component={Logout} />
+      <Route exact path="/b" component={B} />
+    </Switch>
+  );
+}
+
+
+
+const A = () => {
+  return (
+    <div>
+      <h1> This is component A page</h1>
+      <Link to="/b"> B component</Link>
+    </div>
+ 
+  )
+}
+
+const B = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,7 +48,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
-}
+  )
+} 
+
 
 export default App;
